54a7db326e975b6576cd09332636273bcf9a8de9
evo <84800113+evo0616lution@users.noreply.github.com>
2021-12-27 18:17:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo Welcome to the MultiRPC installer, coded by https://github.com/evo0616lution
+echo Downloading dependencies...
+sudo snap install dotnet-runtime-60 --classic
+sudo snap alias dotnet-runtime-60.dotnet dotnet
 echo Building executable...
 dotnet MultiRPC.dll
 chmod +x MultiRPC
