36e9458b6bdd693877dc1f888da8f6be135b20ef
evo <84800113+evo0616lution@users.noreply.github.com>
2021-12-28 10:32:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 echo Welcome to the MultiRPC installer, coded by https://github.com/evo0616lution
 echo Downloading additional software...
 sudo apt update
-sudo apt install git
-echo Getting git version...
-git --version
 echo Building executable...
 chmod +x MultiRPC
 clear
