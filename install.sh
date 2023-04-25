255c7240178a9a7537a530109efbd5f0d4fad4bd
mystic <84800113+pacity@users.noreply.github.com>
2023-04-25 09:09:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Welcome to the MultiRPC installer, coded by https://github.com/evo0616lution
+echo Welcome to the MultiRPC installer, coded by https://github.com/pacity
 echo Downloading additional software...
 sudo apt update
 echo Building executable...
